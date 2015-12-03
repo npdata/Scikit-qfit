4e82b7cf4ce422234b1b77d5a2a1a0de3397f06c
npdata <npdata@bigpond.com>
2015-12-03 16:53:05 +1100

    Another attempt at fixing RTD issue

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ namespace_pkg = ".".join([namespace[-1], package]) if namespace else package
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.abspath('/api'))
+sys.path.append(os.path.abspath('../skqfit'))
 sys.path.append(os.path.abspath('../tests'))
 sys.path.append(os.path.abspath('../example'))
 sys.path.append(os.path.abspath('../../'))
