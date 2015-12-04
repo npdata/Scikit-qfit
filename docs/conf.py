7cecb2a208ffe1a078b74a72ac5962f19690e598
npdata <npdata@bigpond.com>
2015-12-04 15:21:23 +1100

    Incorrect directory added to syspath

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,8 @@ namespace_pkg = ".".join([namespace[-1], package]) if namespace else package
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('../'))
 sys.path.append(os.path.abspath('../skqfit/'))
-sys.path.append(os.path.abspath('../../'))
 
 # -- General configuration -----------------------------------------------------
 
